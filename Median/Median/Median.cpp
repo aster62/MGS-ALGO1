8bb330d9ded70124f1b1eec8b413c5020b206db4
aster62 <andreas.sehr@gmail.com>
2013-11-01 18:18:00 +0100

    Sample Read From File

--- Median/Median/Median.cpp
+++ Median/Median/Median.cpp
@@ -3,22 +3,47 @@
 
 #include "stdafx.h"
 #include "ValueList.h"
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void sampleFromFileToCout(string fileName);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	ValueList createValues(50);
 	int* valueList = createValues.getValuesArr();
 
-	std::cout << "\n" << valueList[0];
+	cout << "\n" << valueList[0];
 
 	createValues.shuffleValueArr();
 
-	std::cout << "\n" << valueList[0];
+	cout << "\n" << valueList[0] << "\n";
 
-	std::cin.get();
+	sampleFromFileToCout("input1.txt");
+	
+	cin.get();
 
 	createValues.~ValueList();
 
 	return 0;
 }
 
+void sampleFromFileToCout(string fileName) {
+	string line;
+	ifstream myfile (fileName);
+	if (myfile.is_open())
+	{
+		while ( getline (myfile,line) ){
+			cout << line << endl;
+		}
+    myfile.close();
+	} 
+	else {
+		cout << "Unable to open file"; 
+	}
+
+	return;
+}
